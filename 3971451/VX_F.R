48ced0bb678df3f5eebc05297d9dd6df03284a94
ISOMORPHISMES <chris.is.fun@gmail.com>
2012-10-29 00:50:11 -0400

    clearer to me

--- 3971451/VX_F.R
+++ 3971451/VX_F.R
@@ -13,7 +13,7 @@ colnames(x) <- names(x)
 x <- x[-1,]
 
 x$"Trade Date" <- as.Date(x$'Trade Date', format='%m/%d/%Y')
-x[,2:11] <- data.frame(sapply(x[,2:11],as.numeric),stringsAsFactors=F)
+x[,2:11] <- data.frame(sapply(x[,2:11],FUN=as.numeric),stringsAsFactors=F)
 x <- xts(x[,2:11], order.by=x$"Trade Date", frequency='d')
 chartSeries(x$Close, subset='last 3 months',theme=chartTheme("white"),name=paste0("VX",mth,yr," Futures"))
 addTA(volatility(last(x$Close,'4 months'),n=n,calc="close",N=252),legend=paste0(n,"-day HV"))
