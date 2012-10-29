5b05140f892562a11639f159ea28c5275743bad2
ISOMORPHISMES <chris.is.fun@gmail.com>
2012-10-29 00:49:09 -0400

    oh duh. they don't use I or L

--- 3971451/VX_F.R
+++ 3971451/VX_F.R
@@ -3,20 +3,18 @@
 
 library(quantmod)
 
-mth <- LETTERS[6:12]
+mth <- LETTERS[c(6:9,11:12,14:15)]
 yr <- 13
 n <- 10 # number of periods for calculating historical volatility in the chart
 
-for (u in url) {
-	u <- paste0("http://cfe.cboe.com/Publish/ScheduledTask/MktData/datahouse/CFE_",mth,yr,"_VX.csv")
-	x <- read.csv(url,header=FALSE, stringsAsFactors=F)
-	colnames(x) <- names(x)
-	x <- x[-1,]
-	
-	x$"Trade Date" <- as.Date(x$'Trade Date', format='%m/%d/%Y')
-	x[,2:11] <- data.frame(sapply(x[,2:11],as.numeric),stringsAsFactors=F)
-	x <- xts(x[,2:11], order.by=x$"Trade Date", frequency='d')
-	chartSeries(x$Close, subset='last 3 months',theme=chartTheme("white"),name=paste0("VX",mth,yr," Futures"))
-	addTA(volatility(last(x$Close,'4 months'),n=n,calc="close",N=252),legend=paste0(n,"-day HV"))
-	last(volatility(last(x$Close,'4 months'),n=n,calc="close",N=252))
-}
+url <- paste0("http://cfe.cboe.com/Publish/ScheduledTask/MktData/datahouse/CFE_",mth,yr,"_VX.csv")
+x <- read.csv(url,header=FALSE, stringsAsFactors=F)
+colnames(x) <- names(x)
+x <- x[-1,]
+
+x$"Trade Date" <- as.Date(x$'Trade Date', format='%m/%d/%Y')
+x[,2:11] <- data.frame(sapply(x[,2:11],as.numeric),stringsAsFactors=F)
+x <- xts(x[,2:11], order.by=x$"Trade Date", frequency='d')
+chartSeries(x$Close, subset='last 3 months',theme=chartTheme("white"),name=paste0("VX",mth,yr," Futures"))
+addTA(volatility(last(x$Close,'4 months'),n=n,calc="close",N=252),legend=paste0(n,"-day HV"))
+last(volatility(last(x$Close,'4 months'),n=n,calc="close",N=252))
