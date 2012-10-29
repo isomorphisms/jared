53ef6a1b729bccac32bb7fdf1acea1ebf67b5316
ISOMORPHISMES <chris.is.fun@gmail.com>
2012-10-29 00:44:16 -0400

    looping test1

--- 3971451/VX_F.R
+++ 3971451/VX_F.R
@@ -7,14 +7,16 @@ mth <- LETTERS[6:12]
 yr <- 13
 n <- 10 # number of periods for calculating historical volatility in the chart
 
-url <- paste0("http://cfe.cboe.com/Publish/ScheduledTask/MktData/datahouse/CFE_",mth,yr,"_VX.csv")
-x <- read.csv(url,header=FALSE, stringsAsFactors=F)
-colnames(x) <- names(x)
-x <- x[-1,]
-
-x$"Trade Date" <- as.Date(x$'Trade Date', format='%m/%d/%Y')
-x[,2:11] <- data.frame(sapply(x[,2:11],as.numeric),stringsAsFactors=F)
-x <- xts(x[,2:11], order.by=x$"Trade Date", frequency='d')
-chartSeries(x$Close, subset='last 3 months',theme=chartTheme("white"),name=paste0("VX",mth,yr," Futures"))
-addTA(volatility(last(x$Close,'4 months'),n=n,calc="close",N=252),legend=paste0(n,"-day HV"))
-last(volatility(last(x$Close,'4 months'),n=n,calc="close",N=252))
+for (u in url) {
+	u <- paste0("http://cfe.cboe.com/Publish/ScheduledTask/MktData/datahouse/CFE_",mth,yr,"_VX.csv")
+	x <- read.csv(url,header=FALSE, stringsAsFactors=F)
+	colnames(x) <- names(x)
+	x <- x[-1,]
+	
+	x$"Trade Date" <- as.Date(x$'Trade Date', format='%m/%d/%Y')
+	x[,2:11] <- data.frame(sapply(x[,2:11],as.numeric),stringsAsFactors=F)
+	x <- xts(x[,2:11], order.by=x$"Trade Date", frequency='d')
+	chartSeries(x$Close, subset='last 3 months',theme=chartTheme("white"),name=paste0("VX",mth,yr," Futures"))
+	addTA(volatility(last(x$Close,'4 months'),n=n,calc="close",N=252),legend=paste0(n,"-day HV"))
+	last(volatility(last(x$Close,'4 months'),n=n,calc="close",N=252))
+}
